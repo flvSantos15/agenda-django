df3c7417d167d21333683f4e66fbd7e65ee6f21e
flvsantos15 <flvsantos300@gmail.com>
2024-09-16 16:50:40 -0300

    chore: add admin model

--- contact/admin.py
+++ contact/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from contact import models
 
-@admin.register()
+@admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-  ...
+  list_display = 'id', 'first_name', 'last_name', 'phone',
+  ordering = 'id', # Se eu precisar que seja decrescente é só colocar o -
+  # list_filter = 'created_date'
+  search_fields = 'id', 'first_name', 'last_name',
+  list_per_page = 10
+  list_max_show_all = 200
+  list_editable = 'first_name', 'last_name',
+  list_display_links = 'id', 'phone',
